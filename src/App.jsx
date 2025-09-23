dbe445dad83738fd68375b636aa5c537b56ed99d
Adriano <adrianoads910@gmail.com>
2025-09-23 20:01:43 -0300

    criação de pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ const App = () => {
   return (
     <main className="min-h-dvh grid place-items-center bg-slate-50">
       <h1 className="text-3xl font-bold text-slate-800">
-        Hello Tailwind + React!
+        Goldfit Academia
       </h1>
     </main>
   );
